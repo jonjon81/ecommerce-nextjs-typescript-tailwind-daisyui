0c504af60d332650a0b4c875f9456df87442e2b6
Jon Atlas <jjjatlas@hotmail.com>
2023-10-28 16:23:54 +0100

    Fix 500 error bug

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Full Stack Issue Tracker
+
 - NextJS
-   - Routing and navigation with App router
-   - NextAuth
+  - Routing and navigation with App router
+  - NextAuth
+  - React Hook Form
 - Typescript
 - Tailwind
 - RadixUI
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,17 +2,28 @@
 
 import { Button, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
+import { useForm, Controller } from 'react-hook-form';
 import 'easymde/dist/easymde.min.css';
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit((data) => console.log(data))}>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register('title')} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+      ></Controller>
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
