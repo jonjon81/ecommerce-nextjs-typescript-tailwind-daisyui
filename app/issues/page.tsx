bb241901b6054ca5774f7f27eb812d05554865f9
Jon Atlas <jjjatlas@hotmail.com>
2023-10-28 14:51:04 +0100

    Setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const Issues = () => {
-  return <div>Issues works</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default Issues;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import '@radix-ui/themes/styles.css';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Theme } from '@radix-ui/themes';
 import Navbar from './Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,8 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html data-theme="winter" lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
